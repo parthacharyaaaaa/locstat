1a36c9ba62696627373637b50872d90955db82a0
Parth <1008parth@gmail.com>
2026-01-23 22:39:38 +0530

    Adds SupportsBuffer protocol

--- cloc/data_structures/typing.py
+++ cloc/data_structures/typing.py
@@ -1,7 +1,11 @@
 import os
 from typing import Any, Literal, MutableMapping, Optional, Protocol, TypeAlias, TypeVar, Union
 
-__all__ = ("OutputMapping", "LanguageMetadata", "OutputFunction", "SupportsMembershipChecks")
+__all__ = ("OutputMapping",
+           "LanguageMetadata",
+           "OutputFunction",
+           "SupportsBuffer",
+           "SupportsMembershipChecks")
 
 OutputMapping: TypeAlias = MutableMapping[str, Union[MutableMapping[str, Any], int, str]]
 LanguageMetadata: TypeAlias = tuple[Optional[bytes], Optional[bytes], Optional[bytes]]
@@ -12,6 +16,11 @@ class OutputFunction(Protocol):
                  filepath: Union[str, os.PathLike[str], int],
                  mode: Literal["w+", "a"] = "w+") -> None:
         ...
+
+class SupportsBuffer(Protocol):
+    def __buffer__(self, flags: int, /) -> memoryview: ...
+
+    # def __release_buffer__(self, buffer: memoryview) -> None: ...
 
 T = TypeVar("T", covariant=True)
 class SupportsMembershipChecks(Protocol[T]):
